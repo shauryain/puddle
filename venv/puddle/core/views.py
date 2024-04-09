8692b69a0b321a6e0f5cb9a1773083b79e5fa98b
shauryain <147681669+shauryain@users.noreply.github.com>
2024-04-09 07:31:37 +0530

    signup page is added

--- venv/puddle/core/views.py
+++ venv/puddle/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from.forms import SignupForm
 
 
 
@@ -19,3 +20,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'signup.html',{
+        'form': form
+    })
