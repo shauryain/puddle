5147cdbe2b2372cf63a1b29ba921d9800e10720a
shauryain <147681669+shauryain@users.noreply.github.com>
2024-04-09 11:44:19 +0530

    project
    working

--- venv/puddle/core/urls.py
+++ venv/puddle/core/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("signup",views.signup,name="signup"),
     # path("login",auth_views.LoginView.as_view(authentication_form=LoginForm), name="login"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login'),
+    path("/Logout", views.logout, name="logout"),
     ]
--- venv/puddle/core/views.py
+++ venv/puddle/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from item.models import Category, Item
 from.forms import SignupForm
+from django.contrib.auth import logout
 
 
 
